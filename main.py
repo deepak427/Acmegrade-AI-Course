0a7b69558786d9482ad7923f547905327bfa6695
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-03-17 17:39:43 +0530

    revised dictionary and sets

--- main.py
+++ main.py
@@ -82,4 +82,36 @@ while x>=1:
     print(x)
     x=x-1
 
+# dictionary
+    
+d1={"a":"some value here", "b":[1,2,3,4], 1:"Hello"}
+print(d1,type(d1))
+print(d1.keys(), d1.values(), d1.items())
+
+d1.update({"b":"foo","4":12})
+print(d1)
+
+words=['apple','bat','bar','atom','book','cook']
+
+by_letter={}
+
+for word in words:
+    letter=word[0]
+    if letter not in by_letter:
+        by_letter[letter]=[word]
+    else:
+        by_letter[letter].append(word)
+
+print(by_letter)
+
+# sets
+
+a={1,2,3,4,5}
+b={3,4,5,6,7,8}
+
+print(a.union(b))
+print(a|b,a&b)
+
+# Comprehension
+
 
