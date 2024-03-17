bf64d8b1b578fa4eb0eceaa14237b21e024ad4af
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-03-17 16:28:45 +0530

    revised data types

---
+++ main.py
@@ -0,0 +1,21 @@
+# data types
+
+x=5
+print(x, type(x))
+
+y=10.5
+print(y, type(y))
+
+z='Deepak Singh'
+print(z, type(z))
+
+tup_a=(4,5,6)
+tup_b=tuple(z)
+print(tup_a, type(tup_a))
+print(tup_b, type(tup_a))
+
+#slicing and indexing
+
+print(tup_b[0:3])
+print(tup_b[:])
+
