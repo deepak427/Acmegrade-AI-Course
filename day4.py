ae086d44d714735e9b8d1ca8c58014221961247a
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-03-26 10:32:49 +0530

    revised numpy array oerations

--- day4.py
+++ day4.py
@@ -1,23 +1,40 @@
-# Pandas
+import numpy as np
 
-import pandas as pd
+print(np.zeros((3,4)))
+print(np.ones((3,4)))
 
-obj=pd.Series([4,7,-5,3])
-print(obj)
+arr1=np.array([1,2,3],dtype=np.float64)
+arr2=np.array([1,2,3],dtype=np.int32)
 
-obj2=pd.Series([4,7,-5,3],index=["a","b","c","d"])
-obj2["d"]=6
-print(obj2["a"])
-print(obj2[obj2>0])
+float_array=arr2.astype(np.float64)
+print(float_array.dtype)
 
-sdata={'Ohio':35000,"Texas":"71000"}
-obj3=pd.Series(sdata)
-print(obj3.to_dict())
+arr=np.array([[1,2,3],[4,5,6]])
+print(arr)
+print(arr*arr)
+print(arr-arr)
+print(arr2>arr1)
 
-data={"states":["Ohio","Ohio","Ohio","Neveda"],
-      "year":[2000,2001,2002,2004],
-      "pop":[1.5,1.7,3.6,2.4]}
-frame=pd.DataFrame(data, columns=["pop","states","year"])
-print(frame.head())
-print(frame.loc[[1],["pop"]])
-print(frame[frame["year"]>2000])
+# # Pandas
+
+# import pandas as pd
+
+# obj=pd.Series([4,7,-5,3])
+# print(obj)
+
+# obj2=pd.Series([4,7,-5,3],index=["a","b","c","d"])
+# obj2["d"]=6
+# print(obj2["a"])
+# print(obj2[obj2>0])
+
+# sdata={'Ohio':35000,"Texas":"71000"}
+# obj3=pd.Series(sdata)
+# print(obj3.to_dict())
+
+# data={"states":["Ohio","Ohio","Ohio","Neveda"],
+#       "year":[2000,2001,2002,2004],
+#       "pop":[1.5,1.7,3.6,2.4]}
+# frame=pd.DataFrame(data, columns=["pop","states","year"])
+# print(frame.head())
+# print(frame.loc[[1],["pop"]])
+# print(frame[frame["year"]>2000])
