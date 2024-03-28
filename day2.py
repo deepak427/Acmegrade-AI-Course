3188391499ebba2b591f95151b790f12f7230639
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-03-28 22:53:37 +0530

    added all files

--- day2.py
+++ day2.py
@@ -1,20 +1,59 @@
-# Nested list comprehension
+# Conditional statements
 
-all_data=[["Deepak", "Bhaiya", "Didi"],
-          ["Mummy", "Papa"]]
+x=10
 
-print(all_data)
+if x>=10:
+    print('Yes')
+else:
+    print('No')
 
-names_of_interest=[]
+# loops
 
-for names in all_data:
-    enough_as=[name for name in names if name.count("a")>=2]
-    names_of_interest.extend(enough_as)
+list_c=[10,20,30,40,50]
 
-print(names_of_interest)
+for i in list_c:
+    print(i)
 
-result=[name for names in all_data for name in names if name.count("a")>=2]
-print(result)
+x=5
 
+while x>=1:
+    print(x)
+    x=x-1
 
+# dictionary
+    
+d1={"a":"some value here", "b":[1,2,3,4], 1:"Hello"}
+print(d1,type(d1))
+print(d1.keys(), d1.values(), d1.items())
 
+d1.update({"b":"foo","4":12})
+print(d1)
+
+words=['apple','bat','bar','atom','book','cook']
+
+by_letter={}
+
+for word in words:
+    letter=word[0]
+    if letter not in by_letter:
+        by_letter[letter]=[word]
+    else:
+        by_letter[letter].append(word)
+
+print(by_letter)
+
+# sets
+
+a={1,2,3,4,5}
+b={3,4,5,6,7,8}
+
+print(a.union(b))
+print(a|b,a&b)
+
+# Comprehension
+
+strings=['a','as','bat','car','dove','python']
+
+c=[x.upper() for x in strings if len(x)>2]
+
+print(c)
--- day4.py
+++ day4.py
@@ -27,22 +27,22 @@ print(arr.T@arr)
 
 import pandas as pd
 
-# obj=pd.Series([4,7,-5,3])
-# print(obj)
+obj=pd.Series([4,7,-5,3])
+print(obj)
 
-# obj2=pd.Series([4,7,-5,3],index=["a","b","c","d"])
-# obj2["d"]=6
-# print(obj2["a"])
-# print(obj2[obj2>0])
+obj2=pd.Series([4,7,-5,3],index=["a","b","c","d"])
+obj2["d"]=6
+print(obj2["a"])
+print(obj2[obj2>0])
 
-# sdata={'Ohio':35000,"Texas":"71000"}
-# obj3=pd.Series(sdata)
-# print(obj3.to_dict())
+sdata={'Ohio':35000,"Texas":"71000"}
+obj3=pd.Series(sdata)
+print(obj3.to_dict())
 
-# data={"states":["Ohio","Ohio","Ohio","Neveda"],
-#       "year":[2000,2001,2002,2004],
-#       "pop":[1.5,1.7,3.6,2.4]}
-# frame=pd.DataFrame(data, columns=["pop","states","year"])
-# print(frame.head())
-# print(frame.loc[[1],["pop"]])
-# print(frame[frame["year"]>2000])
+data={"states":["Ohio","Ohio","Ohio","Neveda"],
+      "year":[2000,2001,2002,2004],
+      "pop":[1.5,1.7,3.6,2.4]}
+frame=pd.DataFrame(data, columns=["pop","states","year"])
+print(frame.head())
+print(frame.loc[[1],["pop"]])
+print(frame[frame["year"]>2000])
