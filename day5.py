33ec995e19d0eeff4db85571474ee02bf4e21b1a
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-03-30 16:17:08 +0530

    unique values and counts

---
+++ day5.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+# unique values and counts
+obj=pd.Series(["a","b","c","d"])
+print(obj)
+print(obj.unique())
+print(obj.value_counts())
+
+#loading data
+
