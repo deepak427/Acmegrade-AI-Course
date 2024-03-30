b671a548f04a578490b7d3346c3c845a3a771394
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-03-30 16:28:14 +0530

    loading data

--- day5.py
+++ day5.py
@@ -9,3 +9,11 @@ print(obj.value_counts())
 
 #loading data
 
+print(pd.read_csv("sample.csv",header=None))
+names=[0,1,2,3,4,5,6,7,8,9]
+print(pd.read_csv('sample.csv',names=names,index_col=3))
+
+# pd.read_csv("sample.txt",sep="\s+")
+
+result = pd.read_csv("sample.csv",skiprows=[0,2,3])
+result.to_csv("out.csv")
