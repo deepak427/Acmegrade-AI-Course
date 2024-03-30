2137c628e6e769f0ca53fbae1cb0f0dc53eb5211
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-03-30 17:22:59 +0530

    null check and fill

--- day5.py
+++ day5.py
@@ -17,3 +17,18 @@ print(pd.read_csv('sample.csv',names=names,index_col=3))
 
 result = pd.read_csv("sample.csv",skiprows=[0,2,3])
 result.to_csv("out.csv")
+
+# Data preprocessing
+
+string_data=pd.Series(["Deepak",np.nan,None,"Hero"])
+print(string_data.isna())
+print(string_data.dropna())
+
+data=pd.DataFrame([[1,4,5.6],[2,np.nan,5],[np.nan,np.nan,np.nan]])
+print(data.dropna(how='all'))
+data[5]=np.nan # data[0] is first coulmn
+print(data)
+print(data.dropna(thresh=2))
+print(data.fillna(0))
+print(data.fillna({0:1,1:2.22222}))
+print(data.fillna(method='ffill',limit=2))
