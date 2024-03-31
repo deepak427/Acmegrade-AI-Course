8d1903941b208c4b38f98dc4c558c129c3c6f366
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-03-31 16:35:06 +0530

    data wrangling

--- day6.py
+++ day6.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 # String Manipulation
 
 val="a, b , gudio"
@@ -9,4 +12,24 @@ print(pieces)
 first, second, third=pieces
 print(first+"::"+second+"::"+third)
 
-print("::")
+print("::".join(pieces))
+
+# Data Wrangling
+
+data=pd.Series(np.random.uniform(size=9),
+               index=[["a","a","a","b","b","b","c","c","c"],
+                      [1,2,3,1,2,3,1,2,3]])
+print(data)
+print(data['b'][3])
+print(data["a":"c"])
+print(data.loc[["a","c"]])
+
+print(data.unstack())
+
+frame=pd.DataFrame(np.arange(12).reshape((4,3)),
+                   index=[["a","a","b","b"],[1,2,1,3]],
+                   columns=[["Ohio","Ohio","Coronada"],["Green","Red","Green"]])
+print(frame)
+
+frame.index.name=["key1","key2"]
+frame.columns.name=[]
