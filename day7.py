56243c96cee4b0d8171ef19ceb98085faf232c5b
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-04-06 17:46:14 +0530

    grouped data frame

--- day7.py
+++ day7.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+import numpy as np
 
 # loading dataset
 data = sns.load_dataset("mpg")
@@ -10,9 +11,9 @@ data['force']=data['weight']*data['acceleration']
 print(data.head())
 
 data['force'].plot.hist(bins=50)
-plt.show()
+# plt.show()
 data['force'].plot.density()
-plt.show()
+# plt.show()
 
 #Regplot and Pairplot
 
@@ -20,14 +21,23 @@ sns.regplot(x = "mpg",
             y = "acceleration",
             data = data,
             dropna = True)
-plt.show()
+# plt.show()
 
-sns.pairplot(data, hue ='weight')
-plt.show()
+# sns.pairplot(data, hue ='weight')
+# plt.show()
 
 #sns.catplot # specify rows and columns
 #Box plot
 
 #Data aggeragation and group operations
-#df.groupby
-#grouped.mean
+
+print(data.groupby(["name", "model_year"])["horsepower"].mean().head())
+print(data.groupby(["model_year"])["name"].count().head())
+print(data.groupby("name", dropna=False).size())
+
+for name, group in data.groupby("model_year"):
+    print(name)
+    print(group)
+
+print(data.groupby(["name", "model_year"])["horsepower"].agg(["mean","std","count"]).head())
+print(data.groupby(["name", "model_year"])["horsepower"].agg([("average","mean"),("std",np.std)]).head())
