5effa8d68c31194032edcd56b757912cf3cfa1f7
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-04-07 16:43:15 +0530

    Frequent Category Imputation

--- day8.py
+++ day8.py
@@ -13,23 +13,48 @@ from sklearn.preprocessing import StandardScaler
 data = sns.load_dataset("mpg")
 
 scalar=StandardScaler()
-scalar.fit(data)
+# scalar.fit(data)
 
-data_scaled=scalar.transform(data)
+# data_scaled=scalar.transform(data)
 
-print(data.head())
+# print(data.head())
 
-sns.kdeplot(data=["age"])
+# sns.kdeplot(data=["age"])
 
 # Min max scalar
 
 from sklearn.preprocessing import MinMaxScaler
 
 scalar=MinMaxScaler()
-scalar.fit(data)
+# scalar.fit(data)
 
-data_scaled=scalar.transform(data)
-print(data.head())
+# data_scaled=scalar.transform(data)
+# print(data.head())
+
+# Handling Missing Values
+# data.isnull.mean
+
+data=data[["mpg","cylinders","displacement"]]
+print(data.isnull().mean())
+
+# median=data.mpg.meadian()
+# print(median)
+
+# Frequent Category Imputation
+
+titanic_data= sns.load_dataset("titanic")
+
+titanic_data= titanic_data[["embark_town","age","fare"]]
+
+print(titanic_data.head())
+
+titanic_data.embark_town.value_counts().sort_values(ascending=False).plot.bar()
+plt.xlabel("Emabark Town")
+plt.ylabel("Number of Passengers")
+
+titanic_data.embark_town.mode()
+
+titanic_data.embark_town.fillna("Southampton",inplace=True)
 
 
 
