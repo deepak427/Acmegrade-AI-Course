41af5407d96fd348ba4292ca1a50eb145314fcce
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-04-14 16:24:18 +0530

    data spilitting, conversion, normalization

--- April/day10.py
+++ April/day10.py
@@ -15,3 +15,32 @@ print(exercise_df.head())
 X= exercise_df.drop(["kind"], axis=1)
 y= exercise_df.filter("kind")
 
+# Converting catrgorical data to numerical
+
+numerical= X.filter(['pulse'])
+
+categorical= X.filter(['diet', 'time'])
+cat_numerical= pd.get_dummies(categorical)
+print(cat_numerical.head())
+
+X= pd.concat([numerical, cat_numerical], axis=1)
+print(X.head())
+
+# Splitting data
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y ,test_size=0.20, random_state=0)
+
+# Data Scaling/ Normalization
+
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+X_train= sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+
+
+
+
+
