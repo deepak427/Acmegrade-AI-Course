bcc3c51dc030caaee99d5ba673d1b787ae19c271
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-04-27 14:57:38 +0530

    normalized data

--- April/day12.py
+++ April/day12.py
@@ -8,3 +8,26 @@ import matplotlib.pyplot as plt
 
 # Load the data
 
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+plt.imshow(X_train[0], cmap="gray")
+plt.show()
+print(y_train[0])
+
+# Preprocess the data
+
+image_height, image_width = 28,28
+
+print(X_train.shape)
+
+X_train=X_train.reshape(60000, image_height*image_width)
+X_test=X_test.reshape(10000, image_height*image_width)
+
+X_train=X_train.astype('float32')
+X_test=X_test.astype('float32')
+
+# normalize data
+
+X_train/=255.0
+X_test/=255.0
+
