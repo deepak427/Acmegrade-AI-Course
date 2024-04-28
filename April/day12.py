3a2be010b04956eea4dd6581dd918ebabe4f2670
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-04-28 09:40:33 +0530

    preprocess data for CNN model

--- April/day12.py
+++ April/day12.py
@@ -31,3 +31,49 @@ X_test=X_test.astype('float32')
 X_train/=255.0
 X_test/=255.0
 
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Building the model
+
+model = Sequential()
+
+model.add(Dense(512, activation="relu", input_shape=(784,)))
+model.add(Dense(512, activation="relu"))
+model.add(Dense(10, activation="softmax"))
+
+# Compile the model
+
+model.compile(loss="categorical_crossentropy",
+              metrics=["accuracy"])
+
+print(model.summary())
+
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_test, y_test))
+
+plt.plot(history.history["accuracy"])
+plt.show()
+
+score=model.evaluate(X_test, y_test)
+print(score)
+
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train=X_train.reshape(60000,28,28,1)
+X_test=X_test.reshape(60000,28,28,1)
+
+X_train=X_train.astype('float32')
+X_test=X_test.astype('float32')
+
+X_train/=255.0
+X_test/=255.0
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# CNN model developement
+
+
+
