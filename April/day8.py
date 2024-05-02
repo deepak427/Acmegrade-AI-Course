3861bdf53a6643cd91ecc24f628d9fa8c4614d8d
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-05-02 21:25:05 +0530

    handling outliners

--- April/day8.py
+++ April/day8.py
@@ -21,8 +21,8 @@ scaled_df = pd.DataFrame(data_scaled, columns=data.columns[:2])
 
 print(scaled_df)
 
-sns.kdeplot(data["sepal_length"])
-plt.show()
+# sns.kdeplot(data["sepal_length"])
+# plt.show()
 
 # Min max scalar
 
@@ -34,31 +34,33 @@ data_scaled=scalar.fit_transform(data_to_scale)
 print(data_scaled[:5])
 
 # Handling Missing Values
-# data.isnull.mean
 
-data=data[["mpg","cylinders","displacement"]]
+data=data[["petal_length","petal_width",]]
 print(data.isnull().mean())
 
-# median=data.mpg.meadian()
-# print(median)
+median=data.petal_length.median()
+print(median)
+data["petal_length"]=data.petal_length.fillna(median)
 
 # Frequent Category Imputation
 
 titanic_data= sns.load_dataset("titanic")
 
 titanic_data= titanic_data[["embark_town","age","fare"]]
-
 print(titanic_data.head())
+print(titanic_data.isnull().mean())
 
 titanic_data.embark_town.value_counts().sort_values(ascending=False).plot.bar()
 plt.xlabel("Emabark Town")
 plt.ylabel("Number of Passengers")
 
-plt.show()
+# plt.show()
 
 titanic_data.embark_town.mode()
 
 titanic_data.embark_town.fillna("Southampton",inplace=True)
+
+print(titanic_data.head())
 
 # Categorical Data Encoding
 
@@ -92,8 +94,8 @@ warnings.filterwarnings("ignore")
 diamond_data= sns.load_dataset("diamonds")
 print(diamond_data.head())
 
-sns.distplot(diamond_data['price'])
-plt.show()
+# sns.distplot(diamond_data['price'])
+# plt.show()
 
 price_range=diamond_data['price'].max()-diamond_data["price"].min()
 
@@ -105,5 +107,18 @@ interval_length= int(price_range/10)
 
 print(lower_interval, upper_interval, interval_length)
 
+total_bins=[i for i in range(lower_interval, upper_interval + interval_length, interval_length)]
+print(total_bins)
+
+bin_labels=['Bin_no' + str(i) for i in range(1, len(total_bins))]
+print(bin_labels)
+
+diamond_data["price_bins"] = pd.cut(diamond_data["price"], bins=total_bins
+                                    , labels=bin_labels, include_lowest=True )
+
+print(diamond_data.head())
+
+diamond_data.groupby("price_bins")["price"].count().plot.bar()
+plt.show()
 # Handling outliers
 
