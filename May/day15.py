9122e42612f2598a7d7945260d3818c5e87f302a
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-05-04 16:25:48 +0530

    created model

--- May/day15.py
+++ May/day15.py
@@ -20,3 +20,42 @@ X_test/=255.0
 
 y_train=to_categorical(y_train,10)
 y_test=to_categorical(y_test,10)
+
+# Creating the model
+
+model=Sequential()
+model.add(Conv2D(32, (3,3), padding='same', input_shape=(32,32,3), activation='relu'))
+model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout (0.25))
+
+model.add(Conv2D (64, (3,3), padding='same', activation='relu'))
+model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout (0.25))
+
+model.add(Flatten())
+model.add(Dense (512, activation= 'relu'))
+model.add(Dropout (8.5))
+model.add(Dense (10, activation='softmax'))
+
+# Compile the model
+
+model.compile(
+    loss="categorical_crossentropy",
+    optimizer="adam",
+    metrics=["acuracy"]
+)
+
+print(model.summary())
+
+# Train the model
+
+model.fit(
+    X_train,
+    y_train,
+    batch_size=32,
+    epoches=25,
+    validation_data=(X_test,y_test),
+    shuffle=True
+)
