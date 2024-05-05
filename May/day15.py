c899f176985661e92ba84c762a1b118148878447
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-05-05 22:37:11 +0530

    tested on a new image

--- May/day15.py
+++ May/day15.py
@@ -67,5 +67,55 @@ f.write_text(model_structure)
 
 # save the model
 
-model.save_weights("data/model_weights.h5")
+model.save_weights("May/data/model_weights.h5")
 
+# Making predictions on image
+
+from tensorflow.keras.models import model_from_json
+from keras.preprocessing import image
+
+class_labels=[
+    "Plans",
+    "car",
+    "Bird",
+    "Cat",
+    "Deer",
+    "Dog",
+    "Frog",
+    "Horse",
+    "Boat",
+    "Truck"
+]
+
+f=Path("model_structure.json")
+model_structure=f.read_text()
+
+model=model_from_json(model_structure)
+
+# Load an image file to test
+
+import matplotlib.pyplot as plt
+from tensorflow.keras.utlis import load_img, img_to_array
+
+img =load_img("dog.png", target_size = (32,32))
+plt.imshow(img)
+plt.show()
+
+# Convert the image to numpy array
+
+import numpy as np
+
+image_to_test = img_to_array(img)
+list_of_images = np.expand_dims(image_to_test, axis=0)
+
+results=model.predict(list_of_images)
+
+single_result=results[0]
+
+most_likely_class_index = int(np.argmax(single_result))
+class_likelihood=single_result[most_likely_class_index]
+
+class_label=class_labels[most_likely_class_index]
+
+print("class label: {}".format(class_label))
+print("class likelihood: {}".format(class_likelihood))
