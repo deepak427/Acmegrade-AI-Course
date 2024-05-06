fdb01fb147b8b0354b8b6e671f2adec795d09baf
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-05-06 18:51:10 +0530

    imported dependencies

--- May/day16.py
+++ May/day16.py
@@ -23,4 +23,17 @@ data = {"intents": [
 
 # import dependencies
 
+import json 
+import string 
+import random
 
+import nltk
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+import tensorflow as tf
+
+from tensorflow. keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+
+nltk.download ("punkt") 
+nltk.download ("wordnet")
